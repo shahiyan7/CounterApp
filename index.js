eb8fef0829c879fc2c4f1df874c237a21a16c775
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-05 19:55:39 +0530

    complete counter app

---
+++ index.js
@@ -0,0 +1,23 @@
+// document.getElementById("count").innerHTML=5
+let counte = 0;
+let welcomeEl = document.getElementById("welcome-el");
+let userName = "shahiyan";
+let greeting = "Welcome Back";
+welcomeEl.innerHTML = greeting + " " + userName + "!";
+welcomeEl.innerHTML += "👋";
+
+function increment() {
+  counte = counte + 1;
+  document.getElementById("count").innerHTML = counte;
+}
+function decrement() {
+  counte = counte - 1;
+  document.getElementById("count").innerHTML = counte;
+}
+function save() {
+  let saveEntries = counte + "- ";
+  document.getElementById("save-el").innerHTML =
+    document.getElementById("save-el").innerHTML + saveEntries;
+  counte = 0;
+  document.getElementById("count").innerHTML = counte;
+}
